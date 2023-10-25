b53e4333eff247602f1d62237be07a85adfdcfca
Richard Udeh <richardudeh03@gmail.com>
2023-10-25 05:46:55 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className="text-center text-red-500 font-bold">
+       heek
+     </div>
+
+    </>
+  )
+}
+
+export default App
